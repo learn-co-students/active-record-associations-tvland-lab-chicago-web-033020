c62f6586aee5b78c748bb69d2eaa5b765cb8861b
Shau Yin Luk <ivan.luk@icloud.com>
2020-04-10 18:48:49 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    fname = "#{self.first_name} #{self.last_name}" 
+  end
+
+  def list_roles  
+    roles = self.characters.map {|cha| "#{cha.name} - "}
+    self.shows.each_with_index do |val, index|
+      roles[index] = "#{roles[index]}#{val.name}"
+    end
+    roles
+  end
 end
