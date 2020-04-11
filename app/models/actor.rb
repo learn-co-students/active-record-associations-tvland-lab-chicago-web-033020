a5c0ff58f98df8524215a9e83941ba9517f7a0fb
aseleidy <ash.etemadi@gmail.com>
2020-04-11 18:36:28 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+    def full_name 
+        "#{self.first_name} #{self.last_name}"
+    end 
+
+    def list_roles 
+        role_show = self.characters.map do |role|
+            id = role.show_id
+            "#{role.name} - #{Show.find(id).name}"
+        end 
+
+        role_show 
+    end 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list 
+        self.characters.map do |character|
+            id = character.actor_id
+            Actor.find(id).full_name
+        end 
+    end 
 end
